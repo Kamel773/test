01629550226825c8843e077b0aa0e6051cad6205
Kamel Alrashedy <kamel773@outlook.com>
2023-05-30 13:57:10 -0400

    Update gen_3.py

--- gen_3.py
+++ gen_3.py
@@ -1,9 +1,4 @@
-def count_identical_items(list1, list2, list3):
-    count = 0
-    min_length = min(len(list1), len(list2), len(list3))
-    
-    for i in range(min_length):
-        if list1[i] == list2[i] == list3[i]:
-            count += 1
-    
-    return count
+def split_list_of_lists(lst):
+    first_elements = [sublist[0] for sublist in lst]
+    second_elements = [sublist[1] for sublist in lst]
+    return [first_elements, second_elements]
