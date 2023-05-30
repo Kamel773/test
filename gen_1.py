46a6b3cdcadef210ae3b05b7def114a167eddef7
Kamel Alrashedy <kamel773@outlook.com>
2023-05-30 13:21:25 -0400

    Update gen_1.py

--- gen_1.py
+++ gen_1.py
@@ -1,8 +1,16 @@
-def flatten_and_sum(lst):
-    flattened_list = []
-    for item in lst:
-        if isinstance(item, list):
-            flattened_list.extend(flatten_and_sum(item))
-        else:
-            flattened_list.append(item)
-    return sum(flattened_list)
+def check_tuple_lengths(*tuples):
+    lengths = set(len(t) for t in tuples)
+    return len(lengths) == 1
+
+# Example usage:
+tuple1 = (1, 2, 3)
+tuple2 = ('a', 'b', 'c')
+tuple3 = (True, False)
+
+print(check_tuple_lengths(tuple1, tuple2, tuple3))  # Output: True
+
+tuple4 = (1, 2, 3, 4)
+tuple5 = ('a', 'b')
+tuple6 = (True, False, True)
+
+print(check_tuple_lengths(tuple4, tuple5, tuple6))  # Output: False
