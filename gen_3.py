e3225df00cc9bcc26bfb383704d706c9ce4bfd40
Kamel Alrashedy <kamel773@outlook.com>
2023-05-30 13:43:29 -0400

    Update gen_3.py

--- gen_3.py
+++ gen_3.py
@@ -1,10 +1,14 @@
-def count_occurrences(string):
-    count = 0
-    index = 0
-    while index < len(string):
-        index = string.find('std', index)
-        if index == -1:
-            break
-        count += 1
-        index += 3  # Move the index after the found occurrence
-    return count
+def newman_conway(n):
+    # Base cases
+    if n == 1:
+        return 1
+    if n == 2:
+        return 1
+    
+    # Recursive case
+    return newman_conway(newman_conway(n - 1)) + newman_conway(n - newman_conway(n - 1))
+
+# Testing the function
+print(newman_conway(1))  # Output: 1
+print(newman_conway(5))  # Output: 3
+print(newman_conway(10)) # Output: 6
