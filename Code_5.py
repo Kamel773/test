74ebbf2f1892495a7215e69bf5cc911743ea59e8
Kamel Alrashedy <kamel773@outlook.com>
2023-06-05 19:45:02 -0400

    Update Code_5.py

--- Code_5.py
+++ Code_5.py
@@ -1,28 +1,15 @@
-import sqlite3
-import json
-def get_table_as_json(db_path, table_name):
-    # Connect to the database
-    conn = sqlite3.connect(db_path)
+def get_table_as_json(db_file, table_name):
+    conn = sqlite3.connect(db_file)
     cursor = conn.cursor()
-    # Execute the query to select all rows from the table
     cursor.execute(f"SELECT * FROM {table_name}")
-    # Fetch all rows from the cursor
     rows = cursor.fetchall()
-    # Get the column names from the cursor description
-    columns = [desc[0] for desc in cursor.description]
-    # Create a list to store the JSON objects
-    data = []
-    # Iterate over the rows and convert each row to a dictionary
+
+    column_names = [desc[0] for desc in cursor.description]
+    results = []
     for row in rows:
-        row_dict = dict(zip(columns, row))
-        data.append(row_dict)
-    # Convert the data list to JSON
-    json_data = json.dumps(data)
-    # Close the database connection
+        row_data = {}
+        for idx, col in enumerate(column_names):
+            row_data[col] = row[idx]
+        results.append(row_data)
     conn.close()
-    return json_data
-# Example usage
-database_path = "example.db"
-table_name = "employees"
-json_result = get_table_as_json(database_path, table_name)
-print(json_result)
+    return json.dumps(results)
