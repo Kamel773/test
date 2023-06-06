f78c61ab660c50f163fcdbdde6c1a6225a53f7b0
Kamel Alrashedy <kamel773@outlook.com>
2023-06-05 22:15:11 -0400

    Update Code_8.py

--- Code_8.py
+++ Code_8.py
@@ -1,7 +1,3 @@
-import json
-import xmltodict
-
-
 def convert_xml_to_json(xml_string):
     """
     Converts XML string to JSON.
@@ -12,6 +8,9 @@ def convert_xml_to_json(xml_string):
     try:
         json_data = json.dumps(xmltodict.parse(xml_string), indent=4)
         return json_data
+    except xmltodict.expat.ExpatError as e:
+        print(f"Error parsing XML: {str(e)}")
+        return None
     except Exception as e:
         print(f"Error converting XML to JSON: {str(e)}")
         return None
