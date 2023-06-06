4a7a889542133034542182615b040c570d0eb5d8
Kamel Alrashedy <kamel773@outlook.com>
2023-06-05 22:13:04 -0400

    Create Code_8.py

---
+++ Code_8.py
@@ -0,0 +1,17 @@
+import json
+import xmltodict
+
+
+def convert_xml_to_json(xml_string):
+    """
+    Converts XML string to JSON.
+
+    :param xml_string: The XML string to convert.
+    :return: The JSON representation of the XML.
+    """
+    try:
+        json_data = json.dumps(xmltodict.parse(xml_string), indent=4)
+        return json_data
+    except Exception as e:
+        print(f"Error converting XML to JSON: {str(e)}")
+        return None
