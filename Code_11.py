8a61cd1ff9f5ebccd4c7c67aa4f13362460642de
Kamel Alrashedy <kamel773@outlook.com>
2023-06-22 20:08:53 -0400

    Update Code_11.py

--- Code_11.py
+++ Code_11.py
@@ -1,17 +1,7 @@
-from sqlalchemy import create_engine, MetaData, Table
+import subprocess
 
-def get_columns_and_types(database_uri, table_name):
-    engine = create_engine(database_uri)
-    meta = MetaData()
-
-    # Load the table from the database
-    table = Table(table_name, meta, autoload_with=engine)
-
-    # Create a dictionary to store column names and types
-    column_data = {}
-
-    # Populate the dictionary with column name as key and type as value
-    for column in table.columns:
-        column_data[column.name] = str(column.type)
-
-    return column_data
+def execute_command(command):
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
+    output, error = process.communicate()
+    returncode = process.returncode
+    return returncode, output, error
