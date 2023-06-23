8e7937ddfcf6bced22f478e188be56d22901300a
Kamel Alrashedy <kamel773@outlook.com>
2023-06-23 00:45:58 -0400

    Add files via upload

---
+++ Interact_with_Bandit.py
@@ -0,0 +1,23 @@
+
+def insert_data(db_config, table_name, data):
+    """Inserts data into a MySQL database.
+    
+    Args:
+        db_config (dict): A dictionary containing the database configuration.
+        table_name (str): The name of the table to insert data into.
+        data (dict): A dictionary containing the data to insert.
+    """
+    # Connect to the database
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+    
+    # Create the SQL query
+    columns = ', '.join(key for key in data.keys())
+    placeholders = ', '.join(['%s'] * len(data))
+    sql = "INSERT INTO %s (%s) VALUES (%s)" % (mysql.escape_string(table_name), columns, placeholders)
+    
+    # Execute the query
+    cursor.execute(sql, list(data.values()))
+    
+    # Commit the changes
+    conn.commit()
